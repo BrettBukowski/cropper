b6f9dc280e54909cf7701a343605166cffb30712
Brett Bukowski <brett.bukowski@gmail.com>
2013-04-11 22:09:47 -0600

    Draw a crop area on the canvas

--- test/cropper.test.js
+++ test/cropper.test.js
@@ -7,9 +7,16 @@ describe('Cropper', function () {
     });
 
     it('Lays a canvas on top of the given image', function () {
-        var crop = new Cropper(document.querySelector('img'));
+        var img = document.querySelector('img');
+        var imgStyle = window.getComputedStyle(img);
+        var crop = new Cropper(img);
         var canvas = document.querySelector('canvas.cropper-canvas');
+
         expect(canvas).to.be.ok;
+        expect(canvas.height).to.equal(parseFloat(imgStyle.getPropertyValue('height')));
+        expect(canvas.width).to.equal(parseFloat(imgStyle.getPropertyValue('width')));
+        expect(parseFloat(canvas.style.top)).to.equal(img.offsetTop);
+        expect(parseFloat(canvas.style.left)).to.equal(img.offsetLeft);
     });
   });
 });
