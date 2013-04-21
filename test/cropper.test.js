f3d508a71256d6a1164a5f34a595ec9e7ccc62c5
Brett Bukowski <brett.bukowski@gmail.com>
2013-04-21 17:54:44 -0600

    Resizable crop pane

--- test/cropper.test.js
+++ test/cropper.test.js
@@ -37,4 +37,26 @@ describe('Cropper', function () {
         expect(document.querySelector('.cropper-crop-pane')).to.not.be.ok;
     });
   });
+
+  describe('get', function () {
+    it('Returns a data uri of the crop region', function (done) {
+      var crop = new Cropper(img);
+      var pane = document.querySelector('.cropper-crop-pane');
+      pane.style.width = '1px';
+      pane.style.height = '1px';
+
+      var output = document.createElement('img');
+      crop._cropChange({ x: 100, y: 100, height: 1, width: 1 });
+      output.src = crop.get();
+      document.body.appendChild(output);
+
+      setTimeout(function () {
+        expect(output.height).to.equal(1);
+        expect(output.width).to.equal(1);
+
+        document.body.removeChild(output);
+        done();
+      });
+    });
+  });
 });
